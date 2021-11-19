fccde5407bab96e0be8503be73d12d2bf163d49d
phuduy2610 <phuduy2610@gmail.com>
2021-11-19 14:23:51 +0700

    Thêm spotlight và xoay khi di chuyển từ điểm sang điểm

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -6,7 +6,8 @@ public class Guard : MonoBehaviour
 {
     public float speed;
     public Transform pathHolder;
-    public float waitTime = 0.3f;
+    public float waitTime = 0.5f;
+    public float turnSpeed = 90;
     private void OnDrawGizmos()
     {
         Vector3 startPosition = pathHolder.GetChild(0).position;
@@ -22,8 +23,9 @@ public class Guard : MonoBehaviour
     void Start()
     {
         Vector3[] waypoints = new Vector3[pathHolder.childCount];
-        for(int i=0;i<waypoints.Length;i++){
-            waypoints[i] = new Vector3(pathHolder.GetChild(i).position.x,transform.position.y,pathHolder.GetChild(i).position.z) ;
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = new Vector3(pathHolder.GetChild(i).position.x, transform.position.y, pathHolder.GetChild(i).position.z);
         }
         StartCoroutine(followPath(waypoints));
     }
@@ -38,20 +40,35 @@ public class Guard : MonoBehaviour
     IEnumerator followPath(Vector3[] path)
     {
         transform.position = path[0];
-        
         int index = 1;
-        while(true){
-            transform.position = Vector3.MoveTowards(transform.position,path[index],speed*Time.deltaTime);
-            if(transform.position == path[index]){
+        transform.LookAt(path[index]);
+        while (true)
+        {         
+            transform.position = Vector3.MoveTowards(transform.position, path[index], speed * Time.deltaTime);
+            if (transform.position == path[index])
+            {
                 index++;
                 index = index % path.Length;
                 yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(Turning(path[index], turnSpeed));
+
             }
             yield return null;
+
         }
-        
+
     }
 
+    IEnumerator Turning(Vector3 destination, float turnSpeed)
+    {
+        Vector3 direction = (destination - transform.position).normalized;
+        float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg;
+        while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y,targetAngle))>0.005f){
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetAngle,turnSpeed*Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
 
+        }
+    }
 
 }
